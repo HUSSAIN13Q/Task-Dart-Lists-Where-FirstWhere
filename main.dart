42d1466fbd13f4c091e42461aeb9f98b337d3fc6
HUSSAIN13Q <hussainalsaffar20@gmail.com>
2024-10-02 15:16:37 +0300

    Dart Lists Where & FirstWhere

--- main.dart
+++ main.dart
@@ -1,7 +1,53 @@
+
 void main(){
-  // Write your code here
-  
-  
-  
-  
+  // sum number
+  var number =[20, 3, 150, 4, 1];
+    int sum=0;
+    number.forEach((num){
+      sum+=num;
+  });
+  print("the sum number is $sum");
+  //  or we can use number.forEach((sum){
+  //   int sum = number.fold(0, (first,num)=>first+num);
+  //   print(sum);
+  //
+  // });
+    
+  //max number
+int max =number[0];
+  number.forEach((n){
+    if(max<n){
+      max = n;
+    }
+
+  });
+  print("the max is $max");
+
+
+ // 🍋 FirstWhere and 🤼‍♂️ orElse 🍋 Where
+print("the minimum age allowed to enter:");
+ print(filterVisitors([33, 6, 15, 19, 1], 16));
+ print("the first odd number:");
+ print(findOdds([88, 44, 54, 99]));  
 }
+
+List filterVisitors( List <int> age,int minimumAge){
+
+return age.where((int output) {
+    return output > minimumAge; }).toList();  
+
+}
+
+int findOdds(List<int> numbers) {
+  return numbers.firstWhere((int number) {
+      return number % 2 != 0;  
+    },
+    orElse: () => 0
+  );
+}
+
+
+
+
+
+
